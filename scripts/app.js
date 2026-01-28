0aaad7abf6d8137d99b4b488cd61ef1416ccf4cd
isaacwide <isaac.aguilard@alumno.buap.mx>
2026-01-27 23:34:58 -0600

    agregado mas js

--- scripts/app.js
+++ scripts/app.js
@@ -5,13 +5,28 @@ elemento.addEventListener("submit", e =>{
     e.preventDefault();
     try{
         let name = document.getElementById("name").value;
+        let phone = document.getElementById("phone").value;
+        let mail = document.getElementById("mail").value;
+        let comentary = document.getElementById("comentary").value;
 
-        if (name == ""){
+
+        if (name == "" && phone == "" && mail == ""){
+
                 alert("informacion no valida")
         }else{
-            alert(`${name}, tendrás información pronto`);
+
+            let data = {
+                "nombre":name,
+                "phone":phone,
+                "mail":mail,
+                "comentary":comentary
+            }
+
+            alert(`${name} gracias por la informacion`);
+
+            elemento.reset();
         }
-    }catch{
+    }catch(error){
         alert('Error no se puedo comunicar ')
     }
 });
